cb30511ffc159a267a0bb277f44e8e9b4506ef7f
Lilian Kasem <liliankasem@gmail.com>
2017-01-26 12:03:21 +0000

    Init commit of project

---
+++ .tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var PBI_CV_E1B40415_2C5D_465F_8148_5B943CA1D176_DEBUG = {
+        name: 'PBI_CV_E1B40415_2C5D_465F_8148_5B943CA1D176_DEBUG',
+        displayName: 'SVG Image Display',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '1.4.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.PBI_CV_E1B40415_2C5D_465F_8148_5B943CA1D176.Visual(options),
+        custom: true
+    };
+}
